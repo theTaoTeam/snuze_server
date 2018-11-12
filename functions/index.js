28e40e9ef86bc14503d2b5528a959a3682c91864
Weston Harper <wes@tao.team>
2018-11-12 02:33:21 -0700

    initial commit

---
+++ functions/index.js
@@ -0,0 +1,13 @@
+require('dotenv');
+const functions = require('firebase-functions');
+const stripe = require('stripe')(process.env.STRIPE_SECRET);
+
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+exports.helloWorld = functions.https.onRequest((request, response) => {
+ response.send("Hello from Firebase!");
+});
