f53531c3ea3bef88f5ed7bd882c18a7d4655d7ef
Weston Harper <wes@tao.team>
2018-11-18 21:07:16 -0700

    feat(index.js): Update createUser to add user to both stripe and firebase
    re #1

--- functions/index.js
+++ functions/index.js
@@ -1,31 +1,45 @@
 require('dotenv').config();
-const functions = require('firebase-functions');
 const stripe = require('stripe')(process.env.STRIPE_SECRET);
-
+const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
+const firestore = new admin.firestore();
+const settings = {
+  timestampsInSnapshots: true
+}
+firestore.settings(settings);
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+
+const testUser = {
+  email: "test@test.com",
+  source: 'tok_visa'
+}
+
+exports.createUser = functions.https.onRequest(async (req, res) => {
+  try {
+    const customer = await stripe.customers.create(testUser);
+    const stripeUser = {
+      authId: 'Y099jN9mo6V31JRVR4MfvoOHzAw2',
+      email: testUser.email,
+      stripeId: customer.id
+    }
+    return admin.firestore().collection('users').doc(stripeUser.authId).set(stripeUser).then(() => {
+      return res.status(303).send('CREATED_USER');
+    }).catch((err) => {
+      return res.status(500).send('ERROR_');
+    })
+  } catch(err) {
+    console.log('THERE WAS AN ERROR CREATING THE USER');
+    console.log(err);
+    res.send(JSON.stringify(err));
+  }
 });
 
-exports.createUser = functions.https.onRequest((req, res) => {
-  console.log(req.body);
-  const customer = createStripeCustomer();
-});
+exports.getUserStats = functions.https.onRequest((req, res) => {
+  res.send("getUserStats");
+})
 
 exports.createSnuzes = functions.https.onRequest((req, res) => {
   console.log("still testing");
   res.send("Testing organization settings");
 });
-
-async function createStripeCustomer() {
-  try {
-    const customer = await stripe.customers.create({
-      email: 'wes@test.com'
-    });
-    console.log(customer);
-  } catch (error) {
-    console.log(error);
-  }
-}
