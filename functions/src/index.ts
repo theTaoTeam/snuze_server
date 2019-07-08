1144a02fafb5b17a84573480dbc359387b926886
Wes Harper <wes.harper@ibotta.com>
2019-07-08 09:27:31 -0600

    WIP added stripe boilerplate

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,25 @@
 import * as functions from 'firebase-functions';
+import { config } from 'dotenv';
+import { resolve } from 'path';
+import * as Stripe from 'stripe';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+config({ path: resolve(__dirname, '../.env') });
+const stripe = new Stripe(process.env.STRIPE_SK as string);
+
+exports.getStripeEphemeralKey = functions.https.onRequest((req, res) => {
+  const stripeVersion: string = req.query.api_version;
+  if (!stripeVersion) {
+    res.status(400).end();
+    return;
+  }
+  // This function assumes that some previous middleware has determined the
+  // correct customerId for the session and saved it on the request object.
+  stripe.ephemeralKeys.create(
+    { customer: req.body.customerId },
+    { stripe_version: stripeVersion }
+  ).then((key: Stripe.ephemeralKeys.IEphemeralKey) => {
+    res.status(200).json(key);
+  }).catch((err: any) => {
+    res.status(500).end();
+  });
+});
