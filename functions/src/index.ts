52999e18f153b6c5dcd54a3a1d2b35935f7acd84
Wes Harper <wes.harper@ibotta.com>
2019-07-09 08:58:50 -0600

    WIP add untested getStripeEphemeral key function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,25 +1,35 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 import { config } from 'dotenv';
 import { resolve } from 'path';
 import * as Stripe from 'stripe';
 
 config({ path: resolve(__dirname, '../.env') });
 const stripe = new Stripe(process.env.STRIPE_SK as string);
+const db = admin.firestore();
 
-exports.getStripeEphemeralKey = functions.https.onRequest((req, res) => {
-  const stripeVersion: string = req.query.api_version;
-  if (!stripeVersion) {
-    res.status(400).end();
-    return;
+exports.getStripeEphemeralKey = functions.https.onCall(async (data, context) => {
+  if (!context.auth) {
+    throw new functions.https.HttpsError('unauthenticated', 'Unauthenticated request');
   }
-  // This function assumes that some previous middleware has determined the
-  // correct customerId for the session and saved it on the request object.
-  stripe.ephemeralKeys.create(
-    { customer: req.body.customerId },
-    { stripe_version: stripeVersion }
-  ).then((key: Stripe.ephemeralKeys.IEphemeralKey) => {
-    res.status(200).json(key);
-  }).catch((err: any) => {
-    res.status(500).end();
-  });
+
+  const stripeVersion: string = data.query.api_version;
+  if (!stripeVersion) {
+    throw new functions.https.HttpsError('invalid-argument', 'Stripe version not found in request');
+  }
+
+  try {
+    const userDocRef = await db.collection('users').doc(context.auth.uid).get();
+    const userData = userDocRef.data();
+    if (!userData) {
+      throw new Error('User doesn\'t exist');
+    }
+    const key: Stripe.ephemeralKeys.IEphemeralKey = await stripe.ephemeralKeys.create(
+      { customer: userData.stripeCustomerId },
+      { stripe_version: stripeVersion }
+    )
+    return key;
+  } catch (e) {
+    throw new functions.https.HttpsError('unknown', 'Error occurred');
+  }
 });
