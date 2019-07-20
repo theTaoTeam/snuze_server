40402cb39b673806f803df0629a8a9d7957e82a1
Wes Harper <wes@tao.team>
2019-07-20 11:47:39 -0600

    Stripe migration to node v8 and ts

---
+++ functions/src/index.ts
@@ -0,0 +1,24 @@
+import * as functions from 'firebase-functions';
+import { config } from 'dotenv';
+import { resolve } from 'path';
+import * as Stripe from 'stripe';
+
+config({ path: resolve(__dirname, '../.env') });
+const stripe = new Stripe(process.env.STRIPE_SK as string);
+
+exports.createStripeUser = functions.https.onCall(async (data, context: functions.https.CallableContext) => {
+  if (!context.auth) {
+    throw new functions.https.HttpsError('unauthenticated', 'Unauthenticated request');
+  }
+  const stripeData = {
+    email: data.email,
+    source: data.source,
+  }
+
+  try {
+    const stripeCustomer = await stripe.customers.create(stripeData);
+    return { id: stripeCustomer.id };
+  } catch(err) {
+    throw new functions.https.HttpsError('aborted', 'error creating stripe customer');
+  }
+});
