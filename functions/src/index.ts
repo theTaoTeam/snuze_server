861dee0f18e323e2a24fa62ba1232f2cddf7fcb9
Weston Harper <wes@tao.team>
2019-07-20 11:46:52 -0600

    redo original function in firebase and node 8+

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,35 +1,24 @@
 import * as functions from 'firebase-functions';
-import * as admin from 'firebase-admin';
 import { config } from 'dotenv';
 import { resolve } from 'path';
 import * as Stripe from 'stripe';
 
 config({ path: resolve(__dirname, '../.env') });
 const stripe = new Stripe(process.env.STRIPE_SK as string);
-const db = admin.firestore();
 
-exports.getStripeEphemeralKey = functions.https.onCall(async (data, context) => {
+exports.createStripeUser = functions.https.onCall(async (data, context: functions.https.CallableContext) => {
   if (!context.auth) {
     throw new functions.https.HttpsError('unauthenticated', 'Unauthenticated request');
   }
-
-  const stripeVersion: string = data.query.api_version;
-  if (!stripeVersion) {
-    throw new functions.https.HttpsError('invalid-argument', 'Stripe version not found in request');
+  const stripeData = {
+    email: data.email,
+    source: data.source,
   }
 
   try {
-    const userDocRef = await db.collection('users').doc(context.auth.uid).get();
-    const userData = userDocRef.data();
-    if (!userData) {
-      throw new Error('User doesn\'t exist');
-    }
-    const key: Stripe.ephemeralKeys.IEphemeralKey = await stripe.ephemeralKeys.create(
-      { customer: userData.stripeCustomerId },
-      { stripe_version: stripeVersion }
-    )
-    return key;
-  } catch (e) {
-    throw new functions.https.HttpsError('unknown', 'Error occurred');
+    const stripeCustomer = await stripe.customers.create(stripeData);
+    return { id: stripeCustomer.id };
+  } catch(err) {
+    throw new functions.https.HttpsError('aborted', 'error creating stripe customer');
   }
 });
